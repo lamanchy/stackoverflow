2c17b9525df4be09ba4837183f55adf27e843a50
lamanchy <lamanchy@gmail.com>
2018-12-29 17:28:25 +0100

    change to 2 intendation to save some space

--- help_cards.py
+++ help_cards.py
@@ -1,60 +1,60 @@
 help_cards = [
-    """\
+  """\
 # HOW TO PLAY THIS GAME:
 def game():
-    # at fist prepare the game:
-    prepare_game()
-    
-    # play round as long as nobody has won
-    while nobody_won():
-        play_round()
+  # at fist prepare the game:
+  prepare_game()
+  
+  # play round as long as nobody has won
+  while nobody_won():
+      play_round()
 
 
 # game preparation is on the other side...\
 """,
-    """\
+  """\
 # do this at the beginning of each game
 def prepare_game():
-    # select output value, it is the same
-    # for whole game
-    global output_value = pop_random_value()
-    
-    # each player gets 5 function cards
-    global players = []
-    for _ in range(len(players)):
-        players.append(
-            [pop_random_fn() for i in range(4)]
-        )\
+  # select output value, it is the same
+  # for whole game
+  global output_value = pop_random_value()
+  
+  # each player gets 5 function cards
+  global players = []
+  for _ in range(len(players)):
+      players.append(
+          [pop_random_fn() for i in range(4)]
+      )\
 """,
-    """\
+  """\
 def nobody_won():
-    # if any player has 0 functions, the game has ended
-    return all(
-        [len(cards) > 0 for cards in players]
-    )\
+  # if any player has 0 functions, the game has ended
+  return all(
+      [len(cards) > 0 for cards in players]
+  )\
 """,
-    """\
+  """\
 def play_round():
-    input_value = pop_random_value()
-    # TODO
-    # each player selects some functions,
-    # to transform input value to output value
-    # as close as possible, the one with closest
-    # value is a winner. Used functions are put
-    # away, each player gets as many new functions
-    # as he used, only winner gets one less.\
+  input_value = pop_random_value()
+  # TODO
+  # each player selects some functions,
+  # to transform input value to output value
+  # as close as possible, the one with closest
+  # value is a winner. Used functions are put
+  # away, each player gets as many new functions
+  # as he used, only winner gets one less.\
 """,
-    """\
+  """\
 # HOW TO USE FUNCTION:
 # function: lambda x: x + 5
-    
+  
 # input value: 3 
 #              ↓
 #       lambda 3: 3 + 5
 #                   ↓
 #     output value: 8\
 """,
-    """\
+  """\
 # HOW SOME MYSTERIOUS THINGS WORK:
 
 5 // 3 == 1 and -5 // 3 == -2
@@ -65,34 +65,34 @@ floor(0.7) == 0 and ceil(0.2) == 1
 "asd"[1] == "s" and "asd"[0:2] == "as"
 "asd"[-1] == "d" and "asd"[::-1] == "dsa"\
 """,
-    """\
+  """\
 # HOW SOME MYSTERIOUS THINGS WORK (part 2):
 
 def gcd(a, b):  # greatest common divisor
-    while b > 0: a, b = b, a % b
-    return a
+  while b > 0: a, b = b, a % b
+  return a
 def lcm(a, b): return abs(a * b) // gcd(a, b)
 def is_prime(n):
-    if n != int(n):  return False
-    if n <= 1:       return False
-    for i in range(2, n):
-        if (n % i) == 0: return False
-    return True\
+  if n != int(n):  return False
+  if n <= 1:       return False
+  for i in range(2, n):
+      if (n % i) == 0: return False
+  return True\
 """,
 ]
 
 
 def get_all_help_cards():
-    tmp = []
-    help = []
-    for i, card in enumerate(help_cards):
-        tmp.append(card)
+  tmp = []
+  help = []
+  for i, card in enumerate(help_cards):
+    tmp.append(card)
 
-        if i + 1 == len(help_cards) or len(tmp) == 2:
-            if len(tmp) == 1:
-                tmp.append("")
+    if i + 1 == len(help_cards) or len(tmp) == 2:
+      if len(tmp) == 1:
+        tmp.append("")
 
-            help.append(tuple(tmp))
-            tmp = []
+      help.append(tuple(tmp))
+      tmp = []
 
-    return help
+  return help
--- rules.py
+++ rules.py
@@ -10,245 +10,245 @@ from math import *
 # napisu program aby delal stack overflow
 
 def get_rules():
-    return {
-        "green": [  # 16 very simple  .... 2-5
-            # 2
-            # these four constants are missing in values
-            lambda x: 7,
-            lambda x: 13,
-            lambda x: 16,
-            lambda x: 19,
-            # 3
-            lambda x: x,
-            lambda x: x + 5,
-            lambda x: x + 11,
-            lambda x: x - 1,
-            # I would not substract more, so the result is never negative (0 in worst case)
+  return {
+    "green": [  # 16 very simple  .... 2-5
+      # 2
+      # these four constants are missing in values
+      lambda x: 7,
+      lambda x: 13,
+      lambda x: 16,
+      lambda x: 19,
+      # 3
+      lambda x: x,
+      lambda x: x + 5,
+      lambda x: x + 11,
+      lambda x: x - 1,
+      # I would not substract more, so the result is never negative (0 in worst case)
 
-            # 4
-            lambda x: 2 * x,
-            lambda x: min(x, 17),
-            lambda x: x // 3,
-            lambda x: max(x, 7),
+      # 4
+      lambda x: 2 * x,
+      lambda x: min(x, 17),
+      lambda x: x // 3,
+      lambda x: max(x, 7),
 
-            # 5
-            lambda x: gcd(x, 24),
-            lambda x: lcm(x, 6),
-            lambda x: x if is_prime(x) else 1,
-            lambda x: x % 5,
-        ],
-        "yellow": [  # 12 math, 8 very simple programs ... 6-10
-            # 6
-            lambda x: x - 25,
-            lambda x: x - 128,
-            lambda x: 151 - x,
-            lambda x: 38 - x,
+      # 5
+      lambda x: gcd(x, 24),
+      lambda x: lcm(x, 6),
+      lambda x: x if is_prime(x) else 1,
+      lambda x: x % 5,
+    ],
+    "yellow": [  # 12 math, 8 very simple programs ... 6-10
+      # 6
+      lambda x: x - 25,
+      lambda x: x - 128,
+      lambda x: 151 - x,
+      lambda x: 38 - x,
 
-            # 7
-            lambda x: x // -6,
-            lambda x: -5 * x,
-            lambda x: 100 // x,
-            lambda x: x % 25,
+      # 7
+      lambda x: x // -6,
+      lambda x: -5 * x,
+      lambda x: 100 // x,
+      lambda x: x % 25,
 
-            # 8
-            lambda x: x * (x // 10),
-            lambda x: ceil(sqrt(abs(x))),
-            lambda x: floor(log2(x)),
-            lambda x: sin(pi * x / 2),
+      # 8
+      lambda x: x * (x // 10),
+      lambda x: ceil(sqrt(abs(x))),
+      lambda x: floor(log2(x)),
+      lambda x: sin(pi * x / 2),
 
-            # 9
-            if_greater_or_less,  # this
-            if_equal_or_not,
-            for_cycle,
-            while_cycle,  # this
+      # 9
+      if_greater_or_less,  # this
+      if_equal_or_not,
+      for_cycle,
+      while_cycle,  # this
 
-            # 10
-            int_from_list,
-            ints_from_list,
-            split_by_int,
-            lambda x: int(str(x)[-1]),
+      # 10
+      int_from_list,
+      ints_from_list,
+      split_by_int,
+      lambda x: int(str(x)[-1]),
 
-        ],
-        "red": [  # KQJ
-            # J can use floats
-            lambda x: x + 0.5,
-            lambda x: -0.5 * x,
-            lambda x: x / 1.5,
-            lambda x: 1 / x,
+    ],
+    "red": [  # KQJ
+      # J can use floats
+      lambda x: x + 0.5,
+      lambda x: -0.5 * x,
+      lambda x: x / 1.5,
+      lambda x: 1 / x,
 
-            # Q float to string and back
-            switch_places,
-            put_and_eval,
-            reverse,
-            subtract_madness,
+      # Q float to string and back
+      switch_places,
+      put_and_eval,
+      reverse,
+      subtract_madness,
 
-            # K
-            rec_subtract,
-            rec_divide,
-            double_rec,
-            rec_multiply,
+      # K
+      rec_subtract,
+      rec_divide,
+      double_rec,
+      rec_multiply,
 
-        ],
-        "white": [
-            # A
-            ackermann,
-            lambda x: min(max(x, 10000), -10000),
-            lambda x: pow(x, -x),
-            lambda x: inf,
-        ]
-    }
+    ],
+    "white": [
+      # A
+      ackermann,
+      lambda x: min(max(x, 10000), -10000),
+      lambda x: pow(x, -x),
+      lambda x: inf,
+    ]
+  }
 
 
 def is_prime(n):
-    if n != int(n):  return False
-    if n <= 1:       return False
+  if n != int(n):  return False
+  if n <= 1:       return False
 
-    for i in range(2, n):
-        if (n % i) == 0:
-            return False
+  for i in range(2, n):
+    if (n % i) == 0:
+      return False
 
-    return True
+  return True
 
 
 def gcd(a, b):
-    while b > 0:
-        a, b = b, a % b
-    return a
+  while b > 0:
+    a, b = b, a % b
+  return a
 
 
 def lcm(a, b):
-    return abs(a * b) // gcd(a, b)
+  return abs(a * b) // gcd(a, b)
 
 
 #  MAX LINE LENGTH 50 SIGNS!!!!!!!!!!!!!!!!!!!!!!
 #  MAX FUNCTION LINES 15 !!!!!!!!!!!!!!!!!!!!!!!!
 
 def if_greater_or_less(x):
-    if x < 10:  return 66
-    if x > 66:  return 10
-    return 42
+  if x < 10:  return 66
+  if x > 66:  return 10
+  return 42
 
 
 #  MAX LINE LENGTH 50 SIGNS!!!!!!!!!!!!!!!!!!!!!!
 #  MAX FUNCTION LINES 15 !!!!!!!!!!!!!!!!!!!!!!!!
 
 def if_equal_or_not(x):
-    if x == 10:
-        x += 66
-    elif x != 66:
-        x -= 10
-    else:
-        return x // 10
+  if x == 10:
+    x += 66
+  elif x != 66:
+    x -= 10
+  else:
+    return x // 10
 
-    return x
+  return x
 
 
 #  MAX LINE LENGTH 50 SIGNS!!!!!!!!!!!!!!!!!!!!!!
 #  MAX FUNCTION LINES 15 !!!!!!!!!!!!!!!!!!!!!!!!
 
 def for_cycle(x):
-    for i in range(floor(x) % 5):
-        x += 10
+  for i in range(floor(x) % 5):
+    x += 10
 
-    return x
+  return x
 
 
 #  MAX LINE LENGTH 50 SIGNS!!!!!!!!!!!!!!!!!!!!!!
 #  MAX FUNCTION LINES 15 !!!!!!!!!!!!!!!!!!!!!!!!
 def while_cycle(x):
-    while x % 4 != 0:
-        x = round(x / 5)
+  while x % 4 != 0:
+    x = round(x / 5)
 
-    return x
+  return x
 
 
 def int_from_list(x):
-    # index:  012345678
-    string = "957812463"
-    return int(string[x % 9])
+  # index:  012345678
+  string = "957812463"
+  return int(string[x % 9])
 
 
 def ints_from_list(x):
-    # index:  0123456789
-    string = "176485923074"
-    first = x % 9
-    second = first + (x % 3) + 1
-    return int(string[first:second])
+  # index:  0123456789
+  string = "176485923074"
+  first = x % 9
+  second = first + (x % 3) + 1
+  return int(string[first:second])
 
 
 def split_by_int(x):
-    string = "19754862315879625413"
-    return len(string.split(str(x)))
+  string = "19754862315879625413"
+  return len(string.split(str(x)))
 
 
 def switch_places(x):
-    string = "{0:.2f}".format(abs(x))
-    a, b = string.split(".")
-    a, b = b, a
-    result = float(a + "." + b)
-    copysign(x, result)
-    return result
+  string = "{0:.2f}".format(abs(x))
+  a, b = string.split(".")
+  a, b = b, a
+  result = float(a + "." + b)
+  copysign(x, result)
+  return result
 
 
 def put_and_eval(x):
-    res = "{0:.2f}".format(x)
-    res.replace('.', '-')
-    return eval(res)
+  res = "{0:.2f}".format(x)
+  res.replace('.', '-')
+  return eval(res)
 
 
 def reverse(x):
-    x = abs(x)
-    string = "{0:.2f}".format(x)
-    return eval(string[::-1])
+  x = abs(x)
+  string = "{0:.2f}".format(x)
+  return eval(string[::-1])
 
 
 def subtract_madness(x):
-    string = "{0:.2f}".format(x)
-    string.replace('.', '--')
-    return eval('-'.join(string))
+  string = "{0:.2f}".format(x)
+  string.replace('.', '--')
+  return eval('-'.join(string))
 
 
 def rec_subtract(x):
-    if x <= 0:
-        return x
+  if x <= 0:
+    return x
 
-    return rec_subtract(x - 30)
+  return rec_subtract(x - 30)
 
 
 def rec_divide(x):
-    if x == 0:
-        return -1
+  if x == 0:
+    return -1
 
-    return -1 + rec_divide(round(x / 3))
+  return -1 + rec_divide(round(x / 3))
 
 
 def double_rec(x):
-    x = abs(x)
-    if x <= 1:
-        return x
+  x = abs(x)
+  if x <= 1:
+    return x
 
-    return double_rec(x // 10) + \
-           double_rec(x // 100)
+  return double_rec(x // 10) + \
+         double_rec(x // 100)
 
 
 def rec_multiply(x):
-    if x % 8 != 0:
-        return x
+  if x % 8 != 0:
+    return x
 
-    return rec_multiply(2 * x) - 1
+  return rec_multiply(2 * x) - 1
 
 
 def ackermann(m, n=None):
-    if n is None:  n = m
-    if m == 0:     return n + 1
+  if n is None:  n = m
+  if m == 0:     return n + 1
 
-    if n == 0:
-        return ackermann(m - 1, 1)
+  if n == 0:
+    return ackermann(m - 1, 1)
 
-    return ackermann(
-        m - 1,
-        ackermann(m, n - 1)
-    )
+  return ackermann(
+    m - 1,
+    ackermann(m, n - 1)
+  )
 
 
 #  MAX LINE LENGTH 50 SIGNS!!!!!!!!!!!!!!!!!!!!!!
@@ -256,27 +256,26 @@ def ackermann(m, n=None):
 
 
 def get_all_functions():
-    fns = []
-    for color in ["green", "yellow", "red", "white"]:
-        for fn in get_rules()[color]:
-            fns.append((fn, color))
+  fns = []
+  for color in ["green", "yellow", "red", "white"]:
+    for fn in get_rules()[color]:
+      fns.append((fn, color))
 
-    return fns
+  return fns
 
 
 if __name__ == "__main__":
-    from timeit import timeit
+  from timeit import timeit
 
-    for fn, color in get_all_functions():
+  for fn, color in get_all_functions():
 
-        source_code = inspect.getsource(fn)
+    source_code = inspect.getsource(fn)
 
-        # remove leading and ending spaces
-        source_code = source_code.strip()
+    # remove leading and ending spaces
+    source_code = source_code.strip()
 
-        if source_code.startswith("lambda") and source_code[-1] == ',':
-            source_code = source_code[:-1]
+    if source_code.startswith("lambda") and source_code[-1] == ',':
+      source_code = source_code[:-1]
 
-        print(source_code)
-        print(timeit("fn(3)", number=1000000, globals=globals()))
-
+    print(source_code)
+    print(timeit("fn(3)", number=1000000, globals=globals()))
